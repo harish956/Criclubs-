264552080b9e079311180a00167609e53316360a
Charan <55391263+Charan017@users.noreply.github.com>
2025-10-10 22:36:24 +0530

    added gluestack

--- CRICLUBS/app/(tabs)/index.tsx
+++ CRICLUBS/app/(tabs)/index.tsx
@@ -1,8 +1,13 @@
-import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native";
+import { HStack } from "@/components/ui/hstack";
+import { Text } from "@/components/ui/text";
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView>
+      <HStack className="justify-between">
+        <Text className="text-2xl font-bold">Home</Text>
+        <Text className="text-2xl font-bold">Test</Text>
+      </HStack>
+    </SafeAreaView>
   );
 }

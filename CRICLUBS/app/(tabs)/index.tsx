ba0d1554d2372870fb55c04431d6b7f5635fd878
Charan <55391263+Charan017@users.noreply.github.com>
2025-10-10 22:59:29 +0530

    mini changes to index.tsx

--- CRICLUBS/app/(tabs)/index.tsx
+++ CRICLUBS/app/(tabs)/index.tsx
@@ -1,9 +1,16 @@
-import { SafeAreaView } from "react-native";
 import { HStack } from "@/components/ui/hstack";
-import { Text } from "@/components/ui/text";
+import { Box } from "@/components/ui/box";
+// import { Text } from "@/components/ui/text";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={{ flex: 1 }}
+      edges={["top", "left", "right"]}
+      className="p-10"
+    >
       <HStack className="justify-between">
         <Text className="text-2xl font-bold">Home</Text>
         <Text className="text-2xl font-bold">Test</Text>
